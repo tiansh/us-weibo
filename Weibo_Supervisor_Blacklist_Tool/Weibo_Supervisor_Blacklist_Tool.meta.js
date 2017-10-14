171fbc90e51b98b6fb4810056a8e30f839711fe1
田生 <tiansheng111@gmail.com>
2017-10-14 20:34:39 +0800

    initial

---
+++ Weibo_Supervisor_Blacklist_Tool/Weibo_Supervisor_Blacklist_Tool.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Weibo Supervisor Blacklist Tool
+// @name:zh     微博监督员批量拉黑工具
+// @namespace   https://github.com/tiansh/
+// @description 帮您自动化解决拉黑微博监督员的烦恼，脚本会在后台帮您逐个慢慢拉黑（不过很抱歉，脚本并没有取消拉黑的功能，敬请见谅）
+// @include     http://www.weibo.com/*
+// @include     http://weibo.com/*
+// @include     http://d.weibo.com/*
+// @include     http://s.weibo.com/*
+// @include     https://www.weibo.com/*
+// @include     https://weibo.com/*
+// @include     https://d.weibo.com/*
+// @include     https://s.weibo.com/*
+// @exclude     http://weibo.com/a/bind/*
+// @exclude     http://weibo.com/nguide/*
+// @exclude     http://weibo.com/
+// @exclude     https://weibo.com/a/bind/*
+// @exclude     https://weibo.com/nguide/*
+// @exclude     https://weibo.com/
+// @version     2017.10.13.1
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_xmlhttpRequest
+// @grant       unsafeWindow
+// @run-at      document-end
+// ==/UserScript==
